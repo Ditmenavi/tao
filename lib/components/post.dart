b98b05b00471c39f2b5a5d88b4bf062a64f96643
NeekOwO <viethailongvuong@gmail.com>
2024-10-16 02:25:48 +0700

    Refactor post component, add voting and copying text functionality

--- lib/components/post.dart
+++ lib/components/post.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/services.dart';
 
 import '../providers/providers.dart';
 
@@ -13,6 +14,19 @@ class APost extends ConsumerStatefulWidget {
 }
 
 class _APostState extends ConsumerState<APost> {
+  String _copyButtonText = 'Copy';
+  Set<String> selected = {''};
+  void updateVoting(Set<String> newSelection) {
+    setState(() {
+      selected = newSelection;
+    });
+    if (selected.isEmpty) {
+      print('No vote');
+    } else {
+      print(selected);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final copypastaAsync = ref.watch(copypastaProvider);
@@ -21,26 +35,133 @@ class _APostState extends ConsumerState<APost> {
       child: copypastaAsync.when(
         data: (Post post) => Card(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(12.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Row(
                       children: [
-                        const CircleAvatar(),
+                        const CircleAvatar(
+                          radius: 25,
+                        ),
                         Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8),
-                          child: Text(post.author),
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(post.author),
+                              Text(post.timestamp),
+                            ],
+                          ),
                         ),
                       ],
                     ),
-                    Text(post.timestamp),
+                    Row(
+                      children: [
+                        IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
+                      ],
+                    )
                   ],
                 ),
               ),
-              Text(post.content),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Text(
+                        post.title,
+                        textAlign: TextAlign.left,
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(12, 0, 12, 12),
+                      child: Text(
+                        post.content,
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(24, 0, 12, 12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SegmentedButton(
+                      segments: const <ButtonSegment<String>>[
+                        ButtonSegment<String>(
+                          value: '+1',
+                          label: Text('0'),
+                          icon: Icon(Icons.keyboard_double_arrow_up_rounded),
+                        ),
+                        ButtonSegment<String>(
+                          value: '-1',
+                          label: Text('0'),
+                          icon: Icon(Icons.keyboard_double_arrow_down_rounded),
+                        ),
+                      ],
+                      selected: selected,
+                      onSelectionChanged: updateVoting,
+                      showSelectedIcon: false,
+                      emptySelectionAllowed: true,
+                    ),
+                    Row(
+                      children: [
+                        TextButton.icon(
+                          onPressed: () async {
+                            await Clipboard.setData(ClipboardData(text: post.content));
+                            // ignore: use_build_context_synchronously
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                duration: Duration(seconds: 3),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                                ),
+                                content: Text(
+                                  'Copied to clipboard',
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            );
+                            setState(() {
+                              _copyButtonText = 'Copied';
+                            });
+                            await Future.delayed(const Duration(seconds: 3));
+                            setState(() {
+                              _copyButtonText = 'Copy';
+                            });
+                          },
+                          style: TextButton.styleFrom(
+                            foregroundColor: Theme.of(context).colorScheme.secondary,
+                          ),
+                          label: Text(_copyButtonText),
+                          icon: const Icon(Icons.copy_rounded),
+                        ),
+                        const SizedBox(width: 8),
+                        TextButton.icon(
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            foregroundColor: Theme.of(context).colorScheme.secondary,
+                          ),
+                          label: const Text('Bookmark'),
+                          icon: const Icon(Icons.bookmark_border_rounded),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
